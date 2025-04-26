5519073f08555e5548fae2216d51cdcc3d55adb5
anshu-kumar27 <anshukumar3552@gmail.com>
2025-04-27 00:50:54 +0530

    final commit

---
+++ readme.md
@@ -0,0 +1,12 @@
+**structure**
+->html : consists of 2 sections left and right - initially divided into 2 sections of screen for larger screen's(flex-row), but for the smaller sections it'll be from top to bottom (flex-column)
+->css : has 3 sections 
+    -- classnames (applying styles on the classes listed in html)
+    -- animations (animation section where i have applied animation on loader icon aka svg and umbrella to show with a fading in effect)
+    -- generic classes to use anywhere anytime
+->script : has all the dynamic logic 
+    -- starting from finding all the elements via "id"
+    -- setting up flags to avoid wrong displays; active color; and triggering something else while something is in pipline working (loading)
+    -- color active and file umbrella color changing section according to the selection
+    -- loading function this helps all the things dynamically change via showing a loading screen
+    -- file upload logic (there are checks for png/jpg aka jpeg) we are using image/jpeg because of image MIME stating jpeg for all jpg pic's as browser doesnt't differentiate between jpg and jpeg, also there are checks to avoid any file greater than 5mb for both of them a warning trigger will occur, and if successfully uploaded file will be marked as uploaded aka true in our flag aswell and loading will be initiated to display the logo :) 
--- script.js
+++ script.js
@@ -10,12 +10,14 @@ const umbrella = document.getElementById('umbrella');
 const displayLogo = document.getElementById('displayLogo');
 const loaderPath = document.getElementById('loaderpath');
 
-let isUploaded = false;
-let current = "blue";
+let isUploaded = false; // file uploaded or not check flag 
+let current = "blue"; // current color flag to update active color
+let isLoading = false; // loading flag to avoid triggering any other function before once this is active
 
 // handling color clicks 
 blue.addEventListener('click',()=>{
-    if(current == "blue") return;
+     // if loading is true or someone tries to click on the same color it'll return avoiding redundant reloads... 
+    if(isLoading || current == "blue") return;
     current = "blue";
     blue.className = " color-boxes bg-blue-active"
     yellow.className = "color-boxes bg-yellow"
@@ -23,11 +25,11 @@ blue.addEventListener('click',()=>{
     BtnText.className ="bg-blue";
     fileUploadIcon.className = "uploader bg-blue";
     container.className = "container bg-blue-100";
-    loader.className = "loader svg-blue";
+    loader.setAttribute("class", "loader svg-blue");
     loadingScreen('./assets/Blue_umbrella.png');
 })
 pink.addEventListener('click',()=>{
-    if(current == "pink") return;
+    if(isLoading || current == "pink") return;
     current = "pink";
     pink.className = "color-boxes bg-pink-active"
     yellow.className = "color-boxes bg-yellow"
@@ -35,11 +37,11 @@ pink.addEventListener('click',()=>{
     BtnText.className ="bg-pink";
     fileUploadIcon.className = "uploader bg-pink";
     container.className = "container bg-pink-100";
-    loader.className = "loader svg-pink";
+    loader.setAttribute("class", "loader svg-pink");
     loadingScreen('./assets/Pink_umbrella.png');
 })
 yellow.addEventListener('click',()=>{
-    if(current == "yellow") return;
+    if(isLoading || current == "yellow") return;  
     current = "yellow";
     yellow.className = "color-boxes bg-yellow-active"
     blue.className = "color-boxes bg-blue"
@@ -47,12 +49,14 @@ yellow.addEventListener('click',()=>{
     BtnText.className ="bg-yellow";
     fileUploadIcon.className = "uploader bg-yellow";
     container.className = "container bg-yellow-100"
-    loader.className = "loader svg-yellow";
+    loader.setAttribute("class", "loader svg-yellow");
     loadingScreen('./assets/Yellow_umbrella.png');
 })
 
 // handling loading screens 
 const loadingScreen = (newUmbrellaImage) => {
+    // loading is marked as true to avoid any other listener getting triggered before this...
+    isLoading= true;
     loader.style.display = 'block'
     umbrella.style.display = 'none'
     displayLogo.style.display = 'none'
@@ -62,17 +66,16 @@ const loadingScreen = (newUmbrellaImage) => {
         loader.style.display = 'none'
         umbrella.style.display = 'block'
         umbrella.className = 'showUmbrella'
-        if(isUploaded){
+        if(isUploaded){ // this will only be set as block if file is uploaded
             displayLogo.style.display = 'block'
             displayLogo.className = 'showUmbrella'
         }
-        if(newUmbrellaImage){
+        if(newUmbrellaImage){ // if the color has been changed the url will be passed making it true
             umbrella.src = newUmbrellaImage;
         }
-    },2000)
+        isLoading=false;
+    },2500)
 }
-
-// handling hover changes to make both of the component behave the same way
 
 // handling uploads 
 fileUploadIcon.addEventListener('click',()=>{
@@ -83,17 +86,32 @@ fileUpload.addEventListener('change',()=>{
     try{
         const file = fileUpload.files[0];
         if(file){
+            // meeting up the requirement of 5mb and png and jpg only 
+            const allowedtypes = ['image/png','image/jpeg']
+            const maxSize = 5 * 1024 * 1024 // 5mb
+            if(!allowedtypes.includes(file.type)){
+                alert("only .png and .jpg files are allowed!")
+                return;
+            }
+            if(file.size > maxSize){
+                alert("file must be less than 5mb")
+                return
+            }
             // selecting a substring of that file only avoiding overflow
-            console.log(file.name)
-            const fileName = file.name.substring(0,12);
+            let fileName = file.name.substring(0,12);
+            if(file.name.length > 12){
+                fileName += "..."
+            }
             BtnText.textContent = fileName;
             const imageUrl = URL.createObjectURL(file);
+            // img src on for logo ON Umbrella
             displayLogo.src = imageUrl;
+            // check of file is uploaded to avoid wrong class allocation
             isUploaded = true;
             loadingScreen();
         }
     }catch(error){
-        console.log("error during file upload")
+        console.log("error during file upload",error)
     }
 })
 
